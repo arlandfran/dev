a169214867802aa52fffed08b09e62add2a1e691
arlandfran <arland.torres@outlook.com>
2025-10-31 11:25:48 +0000

    fix(types): add project.d.ts to resolve @typescript-eslint/no-unsafe-return for JSX return types

---
+++ src/types/project.d.ts
@@ -0,0 +1,10 @@
+// https://github.com/ota-meshi/eslint-plugin-astro/issues/168
+import "astro/astro-jsx";
+
+declare global {
+  namespace JSX {
+    type Element = HTMLElement;
+    // type Element = astroHTML.JSX.Element // We want to use this, but it is defined as any.
+    type IntrinsicElements = astroHTML.JSX.IntrinsicElements;
+  }
+}
