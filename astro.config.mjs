fa24b797dd5238702fed3ea2bb03c26f815fc3d3
arlandfran <arland.torres@outlook.com>
2025-10-31 13:07:57 +0000

    feat(font): add IBM Plex Sans/Mono via Astro's experimental font api

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,30 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        name: "IBM Plex Sans",
+        cssVariable: "--font-ibm-plex-sans",
+        provider: fontProviders.google(),
+        weights: ["400", "600"],
+        subsets: ["latin"],
+        fallbacks: ["sans-serif"],
+      },
+      {
+        name: "IBM Plex Mono",
+        cssVariable: "--font-ibm-plex-mono",
+        provider: fontProviders.google(),
+        weights: ["400 600"],
+        subsets: ["latin"],
+        fallbacks: ["monospace"],
+      },
+    ],
+  },
   site: "https://arland.dev",
   vite: {
     plugins: [tailwindcss()],
